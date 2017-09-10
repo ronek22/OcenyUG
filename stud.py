db8bc15ed88d12f7282345f225bd5925a2fbdede
Jakub Ronkiewicz <jronek3010@gmail.com>
2017-09-10 21:55:19 +0200

    Retrieve first semester grades working

--- stud.py
+++ stud.py
@@ -3,14 +3,17 @@ from lxml import html
 import sys,unicodedata
 import os
 
-loginURL="https://ps.ug.edu.pl:8443/login.web?ajax"
-indeksURL="https://ps.ug.edu.pl:8443/wyniki.web"
-osURL="https://ps.ug.edu.pl:8443/osCzasu.web?ajax=true"
-przedmiotyURL="https://ps.ug.edu.pl:8443/getPrzedmioty.web?ajax=true"
+BASE_URL = "https://ps.ug.edu.pl:8443/"
+loginURL="{}login.web?ajax".format(BASE_URL)
+indeksURL="{}wyniki.web".format(BASE_URL)
+osURL="{}osCzasu.web?ajax=true".format(BASE_URL)
+przedmiotyURL="{}getPrzedmioty.web?ajax=true".format(BASE_URL)
+wynikiURL="{}getWyniki.web?ajax=true".format(BASE_URL)
+wynikinotAjax = "{}getWyniki.web?".format(BASE_URL)
 
 class Student:
 
-    scraped_grades = []
+    sData = []
 
     def __init__(self):
         self.session = requests.session()
@@ -75,11 +78,114 @@ class Student:
         # PRZEJSCIE DO WYBRANEGO SEMESTRU
         response = self.session.get(przedmiotyURL,params=param)
 
+        #GET PRZEDMIOT ID
+        test = response.text.decode('utf-8')
+        positions = list(self.find_all(test,"var urlParams"))
+        pID = self.getPrzedmiotId(positions,test)
+
+        # ZALADOWANIE DO SLOWNIKA NAZW,ID,GUIID PRZEDMIOTOW
         page = html.fromstring(response.content.decode('utf-8'))
         przedmioty = page.xpath('//a')
-        for x in przedmioty:
-            a= "".join(x.itertext())
-            print a
+        gID = page.xpath('//a/@id')
+        gID = [w[11:] for w in gID]
+        for x in range(len(przedmioty)):
+            course= "".join(przedmioty[x].itertext())
+            courseInfo = {
+                'name':course,
+                'pid':pID[x],
+                'gid':gID[x]
+            }
+            self.sData.append(courseInfo)
+
+        for i in range(len(self.sData)):
+            param = {
+                'identyfikatorGUI':self.sData[i]['gid'],
+                'przedmiotId':self.sData[i]['pid']
+            }
+
+            if self.sData[i]['pid']==self.sData[i]['gid'][:-2]:
+                param['osobaId']='0'
+                response = self.session.get(przedmiotyURL,params=param)
+                # print response.status_code
+                page = html.fromstring(response.content.decode('utf-8'))
+                temp_gid = page.xpath('//a/@id')[0][11:]
+                temp_id = int(temp_gid[:-2])+1
+                self.sData[i]['pid']=str(temp_id)
+                self.sData[i]['gid']=temp_gid
+
+                param = {
+                    'identyfikatorGUI':self.sData[i]['gid'],
+                    'przedmiotId':self.sData[i]['pid']
+                }
+
+                response = self.session.get(wynikiURL,params=param)
+                # print response.status_code
+
+                page = html.fromstring(response.content.decode('utf-8'))
+                grade = page.xpath("//td[@style='color: green; vertical-align: middle' or @style='color: black; vertical-align: middle']/text()")[0]
+                grade = "".join(str(grade).split())
+                try:
+                    grade = float(grade)
+                except ValueError:
+                    pass
+
+                self.sData[i]['grade']=grade
+
+            response = self.session.get(wynikiURL,params=param)
+            page = html.fromstring(response.content.decode('utf-8'))
+            grade = page.xpath("//td[@style='color: green; vertical-align: middle' or @style='color: red; vertical-align: middle']/text()")[0]
+            grade = "".join(str(grade).split())
+            try:
+                grade = float(grade)
+            except ValueError:
+                pass
+            self.sData[i]['grade']=grade
+            param = {
+                'osobaid':'0',
+                'semId':semestry[0][1],
+                'wybierzSemKal':semestry[0][0]
+            }
+
+            # PRZEJSCIE DO WYBRANEGO SEMESTRU
+            response = self.session.get(przedmiotyURL,params=param)
+
+        for i in self.sData:
+            print "%45s: %3s" % (i['name'],i['grade'])
+
+        self.average(self.sData)
+
+
+
+
+
+
+
+    def find_all(self, a_str, sub):
+        start = 0
+        while True:
+            start = a_str.find(sub, start)
+            if start == -1: return
+            yield start
+            start += len(sub) # use start += 1 to find overlapping matches
+
+    def getPrzedmiotId(self,pos,stri):
+        p = []
+        for i in pos:
+            a = stri[i+40:i+60]
+            b = filter(str.isdigit,str(a))
+            p.append(b)
+        return p
+
+    def average(self,data):
+        count = 0
+        suma = 0
+        for i in data:
+            if isinstance(i['grade'],float):
+                count+=1
+                suma+=i['grade']
+
+        print "%44s: %3.2f" % ("Srednia: ",(suma/count))
+
 
 
 
